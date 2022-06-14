501d38ca02d3e1195e5baf06b370c67b3b0018cf
Vincent Le Gallic <legallic.vincent@gmail.com>
2022-06-14 17:18:34 +0200

    Add payable function and withdraw

--- contracts/GreetingCard.sol
+++ contracts/GreetingCard.sol
@@ -7,90 +7,115 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./GreetingLib.sol";
 
 contract GreetingCard is IERC721Metadata {
-  using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-  address private nftOwner;
+    address private nftOwner;
 
-  EnumerableSet.AddressSet private signers;
+    EnumerableSet.AddressSet private signers;
 
-  event Signed(address from);
+    event Signed(address from);
 
-  constructor(address to) {
-    nftOwner = to;
-    emit Transfer(address(0), to, 1);
-  }
+    constructor(address to) {
+        nftOwner = to;
+        emit Transfer(address(0), to, 1);
+    }
 
-  function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
-    return interfaceId == type(IERC721).interfaceId
-      || interfaceId == type(IERC721Metadata).interfaceId
-      || interfaceId == type(IERC165).interfaceId;
-  }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId ||
+            interfaceId == type(IERC165).interfaceId;
+    }
 
-  function name() external override view returns (string memory) {
-    return "A";
-  }
+    function name() external view override returns (string memory) {
+        return "Baby Card";
+    }
 
-  /**
-   * @dev Returns the token collection symbol.
-   */
-  function symbol() external override view returns (string memory) {
-    return "A";
-  }
+    /**
+     * @dev Returns the token collection symbol.
+     */
+    function symbol() external view override returns (string memory) {
+        return "GreetingCard";
+    }
 
-  function balanceOf(address owner) external override view returns (uint256) {
-    return owner == nftOwner ? 1 : 0;
-  }
+    function balanceOf(address owner) external view override returns (uint256) {
+        return owner == nftOwner ? 1 : 0;
+    }
 
-  function ownerOf(uint256 tokenId) external override view returns (address) {
-    require(tokenId == 1, "inexistent token");
-    return nftOwner;
-  }
+    function ownerOf(uint256 tokenId) external view override returns (address) {
+        require(tokenId == 1, "inexistent token");
+        return nftOwner;
+    }
 
-  function safeTransferFrom(
-    address from,
-    address to,
-    uint256 tokenId,
-    bytes calldata data
-  ) external override {
-    revert();
-  }
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override {
+        revert();
+    }
 
-  function safeTransferFrom(
-    address from,
-    address to,
-    uint256 tokenId
-  ) external override {
-    revert();
-  }
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {
+        revert();
+    }
 
-  function transferFrom(
-    address from,
-    address to,
-    uint256 tokenId
-  ) external override {
-    revert();
-  }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {
+        revert();
+    }
 
-  function approve(address to, uint256 tokenId) external override {
-    revert();
-  }
+    function approve(address to, uint256 tokenId) external override {
+        revert();
+    }
 
-  function setApprovalForAll(address operator, bool _approved) external override {
-    revert();
-  }
+    function setApprovalForAll(address operator, bool _approved)
+        external
+        override
+    {
+        revert();
+    }
 
-  function getApproved(uint256 tokenId) external override view returns (address) {
-    return address(0);
-  }
+    function getApproved(uint256 tokenId)
+        external
+        view
+        override
+        returns (address)
+    {
+        return address(0);
+    }
 
-  function isApprovedForAll(address, address) external override view returns (bool) {
-    return false;
-  }
+    function isApprovedForAll(address, address)
+        external
+        view
+        override
+        returns (bool)
+    {
+        return false;
+    }
 
-  function sign() public {
-    require(signers.add(msg.sender));
-    emit Signed(msg.sender);
-  }
+    function sign() public payable {
+        require(signers.add(msg.sender));
+        emit Signed(msg.sender);
+    }
+
+    function withdraw() {
+        require(msg.sender == nftOwner);
+        nftOwner.transfer(address(this).balance);
+    }
+
     /**
      * @dev return tokenURI, image SVG data in it.
      */
@@ -100,7 +125,7 @@ contract GreetingCard is IERC721Metadata {
         override
         returns (string memory)
     {
-      require(tokenId == 1);
-      return GreetingLib.tokenURI(signers);
+        require(tokenId == 1);
+        return GreetingLib.tokenURI(signers);
     }
 }
