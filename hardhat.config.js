27588c703eef143ee5a112e4f36ac76be576f02f
libc.so.6 <lakhdar@cometh.io>
2022-06-14 17:32:28 +0200

    add config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,4 +6,18 @@ require("hardhat-gas-reporter");
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/fe615a739b56483f84597cd983434259",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined
+          ? [process.env.PRIVATE_KEY]
+          : [
+              "",
+            ],
+      chainId: 1,
+    },
 };
