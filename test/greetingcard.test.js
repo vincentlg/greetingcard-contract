7f0c13b00ddccfcfdbacddd5166e044b0e9fa9b6
Vincent Le Gallic <legallic.vincent@gmail.com>
2022-06-14 17:11:41 +0200

    manage svg

--- test/greetingcard.test.js
+++ test/greetingcard.test.js
@@ -3,7 +3,7 @@ const { deployments, ethers } = require("hardhat");
 
 describe('GreetingCard', () => {
   it('should.', async () => {
-    const [owner, receiver, signer] = await ethers.getSigners();
+    const [owner, receiver, signer, signer2, signer3, signer4, signer5, signer6, signer7, signer8, signer9, signer10] = await ethers.getSigners();
     const GreetingLib = await ethers.getContractFactory("GreetingLib");
     const greetingLib = await GreetingLib.deploy();
 
@@ -16,6 +16,16 @@ describe('GreetingCard', () => {
 
     const greetingCard = await GreetingCard.deploy(receiver.address);
     await greetingCard.connect(signer).sign();
+
+    await greetingCard.connect(signer2).sign();
+    await greetingCard.connect(signer3).sign();
+    await greetingCard.connect(signer4).sign();
+    await greetingCard.connect(signer5).sign();
+    await greetingCard.connect(signer6).sign();
+    await greetingCard.connect(signer7).sign();
+    await greetingCard.connect(signer8).sign();
+    await greetingCard.connect(signer9).sign();
+    await greetingCard.connect(signer10).sign();
 
     expect(await greetingCard.balanceOf(owner.address)).to.equal(0);
     expect(await greetingCard.balanceOf(receiver.address)).to.equal(1);
